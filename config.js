ea7e1adfc63579d3217b778daaaaefc3bf736ba3
𝘴น𝚖𝔞ꪦ_𝗿ǿⲩ <panelbot10@gmail.com>
2025-09-25 13:17:02 +0000

    Fix

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ if (existsSync(path.join(__dirname, 'config.env'))) {
 process.env.NODE_OPTIONS = '--max_old_space_size=2560'; // 2.5GB memory
 const toBool = (x) => x === 'true';
 
-const DB_URL = '';
+const DB_URL = process.env.DATABASE_URL || '';
 
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || 'KAISEN~KtYliJLa#CbzInslQXZN2j8TFnNJ6A8tISfU6kJLQgYudh02nuf8', // add your session id here
